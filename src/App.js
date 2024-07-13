2b5fd65747b478f0d97a241d4354fa7c15d4c141
Jaka Maver <jm18457@gmail.com>
2024-07-13 07:51:35 +0200

    chore: Change page

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          I successfully deployed a page to GitHub Pages using GitHub Actions!
         </a>
       </header>
     </div>
